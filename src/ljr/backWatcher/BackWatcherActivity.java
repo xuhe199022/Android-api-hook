453ab6d6aa9f4322adaae179501e7b6b1a566791
liujierui <liujierui@gmail.com>
2012-06-14 09:37:20 +0800

    empty project

---
+++ src/ljr/backWatcher/BackWatcherActivity.java
@@ -0,0 +1,13 @@
+package ljr.backWatcher;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BackWatcherActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
